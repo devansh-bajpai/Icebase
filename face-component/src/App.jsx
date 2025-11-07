dab0df335f9369992bc99784f470a98f8bdec729
ankit-3131 <heyyynkit@gmail.com>
2025-11-08 00:26:47 +0530

    Initialization

---
+++ face-component/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import FaceVerify from './FaceStream'
+function App() {
+
+  return (
+    <>
+    <div>
+      <FaceVerify />
+    </div>
+    </>
+  )
+}
+
+
+export default App
---
+++ face-component/src/FaceStream.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FaceVerify() {
+  return (
+    <div>FaceStream</div>
+  )
+}
+
+export default FaceVerify
