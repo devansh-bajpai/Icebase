2e82e8491359f5bdca1421cdc5207d55184db826
Jhalak yadav <yadavjhalak3211@gmail.com>
2025-11-08 04:43:03 +0530

    Error handler added

---
+++ server/middleware/errorHandler.js
@@ -0,0 +1,9 @@
+// middleware/errorHandler.js
+export const errorHandler = (err, req, res, next) => {
+  console.error(`Error: ${err.message}`);
+
+  res.status(err.statusCode || 500).json({
+    success: false,
+    message: err.message || "Server Error",
+  });
+};
