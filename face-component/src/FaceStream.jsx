5a0bef50730434ab6aa4a4bfed1bcf0665538d07
ankit-3131 <heyyynkit@gmail.com>
2025-11-08 00:34:12 +0530

    Face Stream Component init

--- face-component/src/FaceStream.jsx
+++ face-component/src/FaceStream.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import Webcam from 'react-webcam';
+import axios from 'axios';
 
 function FaceVerify() {
   return (
-    <div>FaceStream</div>
-  )
+    <div style={{ textAlign: 'center', marginTop: 20, ...style }}>
+      <h2>Face Verification</h2>
+
+      <Webcam
+        ref={}
+        screenshotFormat="image/jpeg"
+        width={width}
+        height={height}
+        videoConstraints={{ facingMode }}
+        style={{
+          borderRadius: 10,
+          boxShadow: '0 0 10px rgba(0,0,0,0.2)',
+          ...webcamStyle
+        }}
+      />
+
+      <div style={{ marginTop: 10 }}>
+        {isVerifying ? (
+          <p>{loadingText}</p>
+        ) : (
+          <h3>{}</h3>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default FaceVerify
