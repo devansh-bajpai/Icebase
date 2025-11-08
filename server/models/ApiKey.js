19b465620139a68e94c224cd0611d40ec3b7cbff
Jhalak yadav <yadavjhalak3211@gmail.com>
2025-11-08 08:24:00 +0530

    ApiKey model

---
+++ server/models/ApiKey.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const apiKeySchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    key: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const ApiKey = mongoose.model("ApiKey", apiKeySchema);
+
+export default ApiKey;
