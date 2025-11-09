ea43e5c2e1b412c50ab1c85802805c000d57b608
ankit-3131 <heyyynkit@gmail.com>
2025-11-09 07:13:04 +0530

    Example use of the component

--- icebase/src/App.jsx
+++ icebase/src/App.jsx
@@ -1,15 +1,30 @@
-import { useState } from 'react'
-import FaceVerify from './FaceStream'
+import { useState } from "react";
+import WebcamStreamer from "./FaceStream"; // assuming FaceStream exports WebcamStreamer
+
 function App() {
+  // State to hold server response
+
+  // Handle server response
+  const handleServerResponse = (data) => {
+    console.log("Server response received in App:", data);
+  };
 
   return (
     <>
-    <div>
-      <FaceVerify />
-    </div>
+      <div>
+        <WebcamStreamer
+          serverUrl="http://localhost:5000"
+          emitEvent="findUserWithVideo"
+          responseEvent="verify-result"
+          onResponse={handleServerResponse}
+          interval={3000}
+          quality={0.8}
+          showCanvas={false}
+        />
+      </div>
+   
     </>
-  )
+  );
 }
 
-
-export default App
+export default App;
